bc6954b2a2ae3a325b51ef37721a7ade6ba4df65
Cyle Ven Dawson <dawsonc623@gmail.com>
2021-03-12 15:11:00 -0600

    feat: add baseline WebGL 2 support

--- playground/src/ui/index.tsx
+++ playground/src/ui/index.tsx
@@ -1,8 +1,12 @@
 import React    from "react";
 import ReactDOM from "react-dom";
 
+import {
+  Shell
+} from "./shell";
+
 ReactDOM.render(
-  <p>Hello game3ers!</p>,
+  <Shell />,
   document.getElementById(
     "game3"
   )
---
+++ playground/src/ui/shell/ui/main/index.tsx
@@ -0,0 +1,138 @@
+import React, {
+  ReactElement,
+
+  useEffect,
+  useRef,
+  useState
+} from "react";
+
+function createShader(
+  gl2     : WebGL2RenderingContext,
+  type    : GLenum,
+  source  : string
+) : WebGLShader {
+  const shader = gl2.createShader(type);
+
+  if (!shader) {
+    throw new Error("Could not create WebGL shader.");
+  }
+
+  gl2.shaderSource(shader, source);
+  gl2.compileShader(shader);
+
+  const success = gl2.getShaderParameter(shader, gl2.COMPILE_STATUS);
+
+  if (!success) {
+    const error = gl2.getShaderInfoLog(shader) || "Could not compile WebGL shader.";
+    gl2.deleteShader(shader);
+
+    throw new Error(error);
+  }
+
+  return shader;
+}
+
+function createProgram(
+  gl2             : WebGL2RenderingContext,
+  vertexShader    : WebGLShader,
+  fragmentShader  : WebGLShader
+) : WebGLProgram {
+  const program = gl2.createProgram();
+
+  if (!program) {
+    throw new Error("Could not create WebGL program.");
+  }
+
+  gl2.attachShader(program, vertexShader);
+  gl2.attachShader(program, fragmentShader);
+  gl2.linkProgram(program);
+
+  const success = gl2.getProgramParameter(program, gl2.LINK_STATUS);
+
+  if (!success) {
+    const error = gl2.getProgramInfoLog(program) || "Could not link WebGL program.";
+    gl2.deleteProgram(program);
+
+    throw new Error(error);
+  }
+
+  return program;
+}
+
+function compileProgram(
+  gl2                   : WebGL2RenderingContext,
+  vertexShaderSource    : string,
+  fragmentShaderSource  : string
+) : WebGLProgram {
+  const vertexShader = createShader(gl2, gl2.VERTEX_SHADER, vertexShaderSource);
+  const fragmentShader = createShader(gl2, gl2.FRAGMENT_SHADER, fragmentShaderSource);
+
+  return createProgram(gl2, vertexShader, fragmentShader);
+}
+
+function testWebgl(
+  gl2 : WebGL2RenderingContext
+) : void {
+  const gl2Program  = compileProgram(
+    gl2,
+    `#version 300 es
+
+void main() {
+  gl_Position = vec4(0, 0, 0, 1);  // center
+  gl_PointSize = 120.0;
+}
+`,
+    `#version 300 es
+
+precision highp float;
+
+out vec4 outColor;
+
+void main() {
+  outColor = vec4(1, 0, 0, 1);  // red
+}
+`
+  );
+
+  gl2.useProgram(
+    gl2Program
+  );
+
+  gl2.drawArrays(
+    gl2.POINTS,
+    0,
+    1
+  );
+}
+
+export default function ShellMain() : ReactElement {
+  const [
+    context,
+    setContext
+  ] = useState<WebGL2RenderingContext>();
+
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(
+    () => {
+      const newContext  = canvasRef.current?.getContext("webgl2");
+
+      if (newContext) {
+        setContext(
+          newContext
+        );
+
+        testWebgl(
+          newContext
+        );
+      }
+    },
+    []
+  );
+
+  return (
+    <canvas
+      ref = {canvasRef}
+    />
+  );
+}
